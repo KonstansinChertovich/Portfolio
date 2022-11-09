4ccdcea7bc9920efc9837e3fc7e9c1f1b01ec1f4
Konstantsin <shulghin1991@mail.ru>
2022-11-09 22:56:33 +0300

    scrollHandler of portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -182,18 +182,30 @@ function experience() {
 }
 
 function portfolioLinkAnim() {
+    const portfolioWrapper = document.querySelector('.portfolio__wrapper');
     const porfolioLink = document.querySelectorAll('.portfolio__link');
+    const animItemHeight = portfolioWrapper.offsetHeight;
+    const animItemOffset = portfolioWrapper.getBoundingClientRect().top + scrollY;
 
-    let decr = porfolioLink.length / 2 - 1;
-    let time = 500;
-    for(let inc = porfolioLink.length / 2; inc < porfolioLink.length; inc++) {
-        setTimeout(() => {
-            porfolioLink[inc].classList.add('portfolio__link-active');
-        }, time);
-        time+= 200;
-        setTimeout(() => {
-            porfolioLink[decr].classList.add('portfolio__link-active');
-            decr--;    
-        }, time);
-    }
+    window.addEventListener('scroll', scrollHendlerPortfolio);
+
+    function initShowPortfolio() {
+        let decr = porfolioLink.length / 2 - 1;
+        let time = 500;
+        for(let inc = porfolioLink.length / 2; inc < porfolioLink.length; inc++) {
+            setTimeout(() => {
+                porfolioLink[inc].classList.add('portfolio__link-active');
+            }, time);
+            time+= 200;
+            setTimeout(() => {
+                porfolioLink[decr].classList.add('portfolio__link-active');
+                decr--;    
+            }, time);
+        };
+    };
+    function scrollHendlerPortfolio() {
+        if(scrollY > animItemOffset - animItemHeight) {
+            initShowPortfolio();
+        }
+    };
 }
